c14a49ee3ba1c44562d8bd7578b9ef2a675796a7
TranDucc <Tranduc08042k@gmail.com>
2022-06-08 10:45:45 +0700

    this

--- app/src/main/java/com/quangdau/ko/MainActivity.java
+++ app/src/main/java/com/quangdau/ko/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         //this is commen to Fist_new
         //this commen 2
         // now this is commen nè
+        //kakakakaka
 
     }
 }
