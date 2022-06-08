c07fa2908cccd46ef55e5dc576cbe58b35fb8d51
TranDucc <Tranduc08042k@gmail.com>
2022-06-08 10:14:39 +0700

    commen 2

--- app/src/main/java/com/quangdau/ko/MainActivity.java
+++ app/src/main/java/com/quangdau/ko/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //comment neww
         //this is commen to Fist_new
+        //this commen 2
     }
 }
